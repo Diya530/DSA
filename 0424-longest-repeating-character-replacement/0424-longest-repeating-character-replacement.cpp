24d596a3cc240a3186c97b1cde22fda4ac99ee29
Diya <diyabatra000@gmail.com>
2026-06-27 23:19:31 +0530

    Time: 1 ms (76.06%), Space: 10.9 MB (40.96%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        int low=0;
-        int res=0; //max Window(length of longest substring)
-        int maxFreq =0; //max frequency in the hash map
+        int low = 0;
+        int res = 0; //max Window(length of longest substring)
+        int maxFreq = 0; //max frequency in the array
         int n = s.size();
-        unordered_map<char,int> mp;
+        int freq[26] = {0}; 
         
         for(int high=0; high<n; high++){
-            mp[s[high]]++;
-            maxFreq = max(maxFreq,mp[s[high]]);
+            freq[s[high]-'A']++;
+            maxFreq = max(maxFreq, freq[s[high]-'A']);
 
             int windowLength = high-low+1;
             if(windowLength-maxFreq >k){
-                mp[s[low]]--;
+                freq[s[low]-'A']--;
                 low++;
             }
-            
+
             windowLength = high-low+1;
             res = max(res,windowLength);
         }
