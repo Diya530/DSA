7b3b0b23b7e094872a62120c4c476d09d1407bf7
Diya <diyabatra000@gmail.com>
2026-07-15 23:31:09 +0530

    Time: 0 ms (100%), Space: 11 MB (41.66%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -2,24 +2,24 @@ class Solution {
 public:
     int characterReplacement(string s, int k) {
         int low = 0;
-        int res = 0; //max Window(length of longest substring)
-        int maxFreq = 0; //max frequency in the array
+        int res_str = 0;
         int n = s.size();
-        int freq[26] = {0}; 
-        
-        for(int high=0; high<n; high++){
-            freq[s[high]-'A']++;
-            maxFreq = max(maxFreq, freq[s[high]-'A']);
+        int max_freq = 0;
+        int freq[26] = {0};
 
-            int windowLength = high-low+1;
-            if(windowLength-maxFreq >k){
+        for(int high = 0; high<n; high++){
+            freq[s[high]-'A']++;
+            max_freq = max(max_freq , freq[s[high]-'A']);
+            int window_len = high-low+1;
+
+            if(window_len - max_freq > k){
                 freq[s[low]-'A']--;
                 low++;
             }
 
-            windowLength = high-low+1;
-            res = max(res,windowLength);
+            window_len = high-low+1;
+            res_str = max(res_str, window_len);
         }
-        return res;
+        return res_str;
     }
 };
