52561c07061c3bd26c228ac7e58488e33a3c82d4
Diya <diyabatra000@gmail.com>
2026-07-20 00:14:32 +0530

    Time: 0 ms (100%), Space: 10 MB (64.62%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -14,9 +14,9 @@ public:
         ListNode *slow = head;
         ListNode *fast = head;
 
-        while(fast != NULL and fast-> next != NULL){
-            slow = slow ->next;
-            fast = fast -> next -> next;
+        while(fast!= nullptr and fast->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
         }
         return slow;
     }
