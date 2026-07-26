b24f61d5b0468db7c745aa62fbbdf0844311ebc6
Diya <diyabatra000@gmail.com>
2026-07-26 21:37:51 +0530

    Time: 0 ms (100%), Space: 43.7 MB (15.11%) - LeetHub

--- 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.cpp
@@ -3,15 +3,16 @@ public:
     int maxSubarraySumCircular(vector<int>& nums) {
         int n = nums.size();
 
-        int maxend = nums[0] , MaxSum = nums[0];
-        int minend = nums[0] ,  MinSum = nums[0];
-        int NormalMax = nums[0],  CircularMax = nums[0];
+        int maxend = nums[0], MaxSum = nums[0];
+        int minend = nums[0], MinSum = nums[0];
+
+        int NormalMax = nums[0], CircularMax = nums[0];
         int sum = nums[0];
 
         for(int i = 1; i<n; i++){
 
-            maxend = max(maxend+nums[i], nums[i]);
-            minend = min(minend+nums[i], nums[i]);
+            maxend = max(maxend + nums[i], nums[i]);
+            minend = min(minend + nums[i], nums[i]);
 
             MaxSum = max(MaxSum, maxend);
             MinSum = min(MinSum, minend);
@@ -19,10 +20,11 @@ public:
             sum += nums[i];
         }
         NormalMax = MaxSum;
-        CircularMax = sum - MinSum;
+        CircularMax = sum-MinSum;
 
         if(NormalMax < 0) return NormalMax;
 
         return max(NormalMax, CircularMax);
+
     }
 };
