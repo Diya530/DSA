870856d14b3bb77ffd4ddcf0948865b627d086b0
Diya <diyabatra000@gmail.com>
2026-08-02 22:25:20 +0530

    Time: 0 ms (100%), Space: 24 MB (25.42%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.cpp
@@ -7,15 +7,17 @@ public:
 
         for(int i = 1; i<nums.size(); i++){
             PrefixSum[i] += PrefixSum[i-1];
+            //OR
+            //PrefixSum[i] = PrefixSum[i-1] + nums[i];
         } 
     }
     
     int sumRange(int left, int right) {
         if(left == 0) return PrefixSum[right];
-        int RightSum = PrefixSum[right];
-        int LeftSum = PrefixSum[left - 1];
+        //int RightSum = PrefixSum[right];
+        //int LeftSum = PrefixSum[left - 1];
 
-        return RightSum - LeftSum;
+        return PrefixSum[right] - PrefixSum[left - 1];
     }
 };
 
