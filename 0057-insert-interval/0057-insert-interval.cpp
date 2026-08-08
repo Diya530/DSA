5ce8c4a4612d8b11c3318b60bb7dc26bda4afe60
Diya <diyabatra000@gmail.com>
2026-08-09 00:27:30 +0530

    Time: 3 ms (30.77%), Space: 21.7 MB (76.89%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -1,47 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        
         vector<vector<int>> res;
-        bool insert = false;
+        int i = 0;
+        int n = intervals.size();
 
-        for(int i=0; i<intervals.size(); i++){
-            int start = intervals[i][0];
-
-            if(insert == false and start>=newInterval[0]){
-                res.push_back(newInterval);
-                insert = true;
-            }
-
-            res.push_back({intervals[i][0], intervals[i][1]});
-
+        // 1. Add all intervals coming BEFORE the newInterval
+        while (i < n && intervals[i][1] < newInterval[0]) {
+            res.push_back(intervals[i]);
+            i++;
         }
-        if(insert==false){
-            res.push_back(newInterval);
+
+        // 2. Merge all overlapping intervals with newInterval
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = min(newInterval[0], intervals[i][0]);
+            newInterval[1] = max(newInterval[1], intervals[i][1]);
+            i++;
         }
-        vector<vector<int>> finalRes;
-        int start1 = res[0][0];
-        int end1 = res[0][1];
+        res.push_back(newInterval);
 
-        for(int i=1; i<res.size(); i++){
-            int start2 = res[i][0];
-            int end2 = res[i][1];
-
-            if(end1 >= start2){
-                start1 = start1;
-                end1 = max(end1,end2);
-                continue;
-            }
-            else{
-                finalRes.push_back({start1,end1});
-                start1 = start2;
-                end1 = end2;
-            }
+        // 3. Add all the remaining intervals
+        while (i < n) {
+            res.push_back(intervals[i]);
+            i++;
         }
-        finalRes.push_back({start1,end1});
 
-        return finalRes;
-        
+        return res;
     }
-    
 };
